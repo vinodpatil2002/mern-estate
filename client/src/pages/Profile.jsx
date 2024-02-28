4872d9b922203cca1fa9ed01ef9cd505afb38e65
Vinod N Patil <83590514+vinodpatil2002@users.noreply.github.com>
2024-02-29 02:30:43 +0530

    Add file upload functionality to Profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,56 @@
 // import React from 'react'
 import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import  {getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { app } from "../firebase";
+// import { get, set } from "mongoose";
 
-function Profile() {
+export default function Profile() {
   const { currentUser } = useSelector((state) => state.user);
+  const [file, setFile] = useState(undefined);
+  const fileRef = useRef(null);
+  const [filePer, setFilePer] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+  useEffect(() => {
+    if(file){
+      handleFileUpload(file);
+    }
+  }, [file]);
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePer(Math.round(progress));    
+  }, 
+  (error) => {
+    setFileUploadError(true);
+  },
+  () =>{
+    getDownloadURL(uploadTask.snapshot.ref).then
+    ((downloadURL) =>
+      setFormData({...formData, avatar: downloadURL})
+    );
+  }
+  );
+  };
+
   return (
     <div className="p-2 max-w-lg mx-auto">
       <h1 className="text-3xl font-bold text-center my-7 ">Profile</h1>
       <form className="flex flex-col gap-4">
-        <img src={currentUser.avatar} alt="profile"  className="rounded-full w-40 h-40 object-cover cursor-pointer self-center"/>
+        <input onChange={(e) =>{
+          setFile(e.target.files[0])
+        }} type="file" ref={fileRef} hidden accept="image/*" />
+        <img onClick={()=>fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt="profile"  className="rounded-full w-40 h-40 object-cover cursor-pointer self-center"/>
+        <p className="text-sm self-center">{fileUploadError ? (<span className="text-red-700">Error image Uploaded (Image max size must be of 2MB) </span>)
+        : filePer > 0 && filePer < 100 ? (<span className="text-slate-700"> 
+        {`Uploading ${filePer}%`} </span>) : filePer===100 ? (<span className="text-green-700">Image Successfully Uploaded</span>) : ""
+      }</p>
         <input type="text" placeholder='username' id="username" className="border p-3 rounded-lg" />
         <input type="email" placeholder='email' id="email" className="border p-3 rounded-lg" />
         <input type="password" placeholder='password' id="password" className="border p-3 rounded-lg" />
@@ -19,7 +62,4 @@ function Profile() {
       </div>
     </div>
   )
-  
 }
-
-export default Profile
