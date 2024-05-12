98e196925881187cb35cbc1841574337b7cde493
vinodpatil2002 <vinodnspatil@gmail.com>
2024-05-12 11:25:38 +0530

    chore: Update currency symbol in ListingItem component

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -18,7 +18,7 @@ export default function ListingItem({listing}) {
             </div>
             <p className='text-sm text-gray-600 line-clamp-2'>{listing.description}</p>
             <p className='text-slate-500 mt-2 font-semibold '>
-                $
+                ₹
             {
                 listing.offer ? listing.discountPrice.toLocaleString('en-US') : listing.regularPrice.toLocaleString('en-US')
             }
