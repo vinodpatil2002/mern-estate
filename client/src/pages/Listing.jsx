eb6dc335699142e4f864c3d501dee64540e35556
vinodpatil2002 <vinodnspatil@gmail.com>
2024-05-12 11:31:46 +0530

    chore: Update currency symbol in Listing component

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -98,7 +98,7 @@ export default function Listing() {
                 </p>
                 {
                     listing.offer && (
-                        <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>${+listing.regularPrice - +listing.discountPrice} Off</p>
+                        <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>₹{+listing.regularPrice - +listing.discountPrice} Off</p>
                     )
                 }
             </div>
